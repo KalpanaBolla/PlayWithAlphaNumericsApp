d75ce43e3b4e1b9574fba06c4cfabf2fc7c0de0b
KalpanaBolla <kalpana.ykbs38@gmail.com>
2023-09-13 18:11:06 -0500

    Initial Commit

---
+++ PlayWithAlphaNumericsApp/PlayWithAlphaNumVC.swift
@@ -0,0 +1,29 @@
+//
+//  PlayWithAlphaNumVC.swift
+//  PlayWithAlphaNumericsApp
+//
+//  Created by Kalpana Bolla on 9/13/23.
+//
+
+import UIKit
+
+class PlayWithAlphaNumVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var headerLBL: UILabel!
+    
+    @IBOutlet weak var firstNumLBL: UIStackView!
+    
+    @IBOutlet weak var secondNumLBL: UIStackView!
+    
+    @IBAction func firstStrTF(_ sender: Any) {
+    }
+    
+    
+    
+    
+}
